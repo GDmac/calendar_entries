514d8ca400524015a0d3ec196677d628ee14df65
GDmac <grdalenoort@gmail.com>
2011-12-26 22:42:47 +0100

    Weekview test

--- system/expressionengine/third_party/calendar_entries/pi.calendar_entries.php
+++ system/expressionengine/third_party/calendar_entries/pi.calendar_entries.php
@@ -23,7 +23,10 @@ class Calendar_entries
 		// 'template'   => '',       // A string containing your calendar template. See the template section below.
 		// 'local_time' => time(),   // A Unix timestamp corresponding to the current time.
 	);
-
+	public $weekdays = array(
+		
+	);
+	
 
 	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 	public function __construct()
@@ -68,6 +71,115 @@ class Calendar_entries
 		return $this->EE->calendar->generate($year,$month,$data);
 
 	}
+
+
+	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	public function week()
+	{
+		// first day of the week, parameter start_day (sunday, monday, etc)
+		$start_day = date('N', strtotime($this->EE->TMPL->fetch_param('start_day','sunday') ) );
+		if($start_day == false) $start_day = 7;
+
+		// current date in parameter start
+		$start = strtotime($this->EE->TMPL->fetch_param('start', false));
+		if($start===false) $start = $this->EE->localize->set_localized_time($this->EE->localize->now);
+
+		// set start to first day of the week
+		$weekday = date('N',$start);
+
+		if( $weekday != $start_day )
+		{
+			if($weekday < $start_day) $weekday = $weekday + 7;
+			$start = $start - ($weekday - $start_day) * 86400;
+		}
+
+		// replace tags for start_on and stop_before parameters
+		$calendar_entries_start = date('Y-m-d 00:00', $start);
+		$calendar_entries_end = date('Y-m-d 00:00', $start + (7 * 86400));
+		
+		$this->tagdata = str_replace('{calendar_entries_start}', $calendar_entries_start, $this->tagdata);
+		$this->tagdata = str_replace('{calendar_entries_end}', $calendar_entries_end, $this->tagdata);
+		
+		// TODO: move to session var, instead of altering the tag :-)
+		$this->tagdata = str_replace('{exp:calendar_entries:week_items', "{exp:calendar_entries:week_items start=\"$start\"", $this->tagdata);
+
+		return $this->tagdata;
+	}
+
+
+	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	public function week_items()
+	{
+		// from session or tag
+		$start = $this->EE->TMPL->fetch_param('start');
+		$today = date('Y-m-d',$this->EE->localize->set_localized_time($this->EE->localize->now));
+		
+		$data = array();
+		
+		foreach( $this->EE->TMPL->var_pair as $key => $params)
+		{
+			if (strncmp($key, 'item', 4) == 0)
+			{
+				$date    = (int) $params['entry_date'];
+				$tmp     = preg_match("/{".preg_quote($key)."}(.*?){\/item}/s", $this->tagdata, $matches);
+				$content = $matches[1];
+
+				$data[date('Y-m-d',$date)][] = array(
+					'date'=>$date, 
+					'content'=>$content
+				);
+			}
+
+			if (strncmp($key, 'calendar_template', 17) == 0)
+			{
+				$tmp = preg_match("/{".preg_quote($key)."}(.*?){\/calendar_template}/s", $this->tagdata, $matches);
+				$this->config['template'] = $matches[1];
+			}
+
+		}
+
+		$ret = '';
+
+		// output week table
+		$ret .= '<table class="calendar week">'.PHP_EOL;
+		$ret .= '<tr>'.PHP_EOL;
+
+		// header
+		for ($i = 0; $i <= 6; $i++)
+		{
+			$ret .=  "<th>".date('D d M',($start + ($i  * 86400)))."</th>";
+		}
+
+		$ret .= '</tr>';
+		$ret .= '<tr>';
+
+		// data
+		for ($i = 0; $i <= 6; $i++)
+		{
+			$day = date('Y-m-d', ($start + ($i  * 86400)));
+
+			$ret .= '<td><div'.($day==$today?' class="today"':'').'>';
+
+			if(isset($data[$day]))
+			{
+				foreach($data[$day] as $item) $ret .= $item['content'];
+			}
+
+			$ret .=  PHP_EOL.'</div></td>'.PHP_EOL;
+		}
+		$ret .= '</tr></table>';
+
+		$ret .= '<p>';
+		$ret .= '<a href="./'.date('Y-m-d',$start-(7*86400)).'">« prev</a>';
+		$ret .= '&nbsp;';
+		$ret .= '<a href="./'.date('Y-m-d',$start+(7*86400)).'">next »</a>';
+		$ret .= '</p>';
+
+		return $ret;
+
+	}
+
+
 }
 /* End of file */ 
 /* Location: ./system/expressionengine/third_party/ */ 
